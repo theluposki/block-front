11268f397be9b4fa5e26685160f0f34cbcf070aa
theluposki <theluposki@gmail.com>
2025-02-03 10:36:52 -0300

    enc

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,17 +1,16 @@
 import CryptoJS from 'crypto-js';
-import config from '../config.js'
 
 function generateHash (data) {
   return CryptoJS.SHA256(data).toString();
 };
 
-function encryptValue(value) {
-  const encryptedValue = CryptoJS.AES.encrypt(JSON.stringify(value), generateHash(config.SECRETKEY)).toString();
+function encryptValue(value, key) {
+  const encryptedValue = CryptoJS.AES.encrypt(JSON.stringify(value), generateHash(key)).toString();
   return encryptedValue;
 }
 
-function decryptValue(encryptedValue) {
-  const decryptedBytes = CryptoJS.AES.decrypt(encryptedValue, generateHash(config.SECRETKEY));
+function decryptValue(encryptedValue, key) {
+  const decryptedBytes = CryptoJS.AES.decrypt(encryptedValue, generateHash(key));
   const decryptedValue = decryptedBytes.toString(CryptoJS.enc.Utf8);
   return JSON.parse(decryptedValue);
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,46 @@
+<script setup>
+import { ref } from 'vue'   
+import { encryptValue, decryptValue } from '../utils/crypto.js'
+
+const key = ref("")
+const text = ref("lucas")
+const encText = ref("")
+
+const handleEnc = () => {
+  encText.value = encryptValue(encText.value)
+}
+</script>
+
 <template>
   <div class="page">
-    Home
+    <div class="container">
+      <fieldset>
+        <legend>Encripted</legend>
+        <input type="text" class="input" placeholder="chave">
+        <textarea class="textarea" v-model="text" placeholder="texto aqui"></textarea>
+        <textarea class="textarea" v-model="encText" placeholder="texto encriptado aqui"></textarea>
+        <button class="btn" @click="handleEnc">Enc</button>
+      </fieldset>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--p);
+  padding: var(--p);
+}
+
+fieldset {
+  display: flex;
+  flex-direction: column;
+  gap: var(--p);
+}
+
+.textarea {
+  resize: none;
+  padding: var(--p);
+}
+</style>
